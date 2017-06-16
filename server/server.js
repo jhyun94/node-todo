3d975a19e6daf2bd54533604b1aceaec1bde21ee
jhyun94 <hyunjonathan@gmail.com>
2017-06-16 12:05:16 -0500

    route GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,11 +4,13 @@ var bodyParser = require('body-parser');
 var mongoose = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectID} = require('mongodb')
 
 var app = express();
 
 app.use(bodyParser.json());
 
+//create a todo
 app.post('/todos', (req,res) => {
   var text = req.body.text;
 
@@ -22,7 +24,7 @@ app.post('/todos', (req,res) => {
     res.status(400).send();
   })
 })
-
+// read all todos
 app.get('/todos', (req,res) => {
   Todo.find().then((todos) => {
     res.send({todos});
@@ -30,6 +32,20 @@ app.get('/todos', (req,res) => {
     res.status(400).send();
   })
 })
+
+//read an individual todo
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(404).send()
+  })
+})
+
 
 
 app.listen(8000, () => {
